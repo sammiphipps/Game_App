2dfc45d8b8ca7d970eaba40e0292af926f340e5f
Sydney Werbach <sydwer@gmail.com>
2019-11-12 09:54:50 -0700

    Added another seed for each of the game categories.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -35,6 +35,16 @@ Game.create(
 )
 
 Game.create(
+    name: "Old Maid",
+    category: card,
+    description: "A Victorian card game for two or more players probably deriving from an ancient gambling game in which the loser pays for the drinks.",
+    basic_rules: "Each player takes turns offering his or her hand face-down to the person on his or her left. That person selects a card without looking and adds it to his or her hand.The objective of the game is to continue to take cards, discarding pairs, until no more pairs can be made. The player with the card that has no match is \"stuck with the old maid\" and loses.",
+    image_link: "https://upload.wikimedia.org/wikipedia/commons/7/71/Old_Maid_2.jpg",
+    min_player: 2,
+    max_player: 12
+)
+
+Game.create(
     name: "Monopoly",
     category: board,
     description: "Players move around the game board: buying and trading properties and developing them with houses and hotels",
@@ -52,6 +62,16 @@ Game.create(
     image_link: "https://www.catan.com/files/pageimages/dye_catan_150407_0564.jpg",
     min_player: 2,
     max_player: 4
+)
+
+Game.create(
+    name: "Checkers",
+    category: board,
+    description: " A strategy game involving diagonal moves of uniform game pieces and mandatory captures by jumping over opponent pieces.",
+    basic_rules: "Players take tunrs moving one of their pieces. A turn can involve moving a single space, or \"jumping\" an opponet's pieces. The goal is to reach the other side of the board and be \"kinged\", granting a piece more movement options. A player wins, when they capture the entirety of the other player's pieces.",
+    image_link: "https://images-na.ssl-images-amazon.com/images/I/81qK4Te%2B6cL._SX466_.jpg",
+    min_player: 2,
+    max_player: 2
 )
 
 Game.create(
@@ -75,6 +95,15 @@ Game.create(
 )
 
 Game.create(
+    name: "Thumper",
+    category: drinking,
+    description: "Players circle a table and take turns exchanging hand signs.",
+    basic_rules: "Each player chooses a unique hand sign. A player begins the round by doing their hand signal then someone else’s. The player whose hand signal was just done must repeat his own hand signal then the signal of another player.This continues until someone doesn’t realize it’s their turn or makes a signal that doesn’t belong to anyone, at which point htey drink.",
+    image_link: "https://thebestdrinkinggames.com/wp-content/uploads/2015/11/fun-drinking-games.jpg",
+    min_player: 4
+)
+
+Game.create(
     name: "Ping Pong",
     category: other,
     description: "Sport where players hit a ball across the table using small rackets.",
@@ -87,9 +116,47 @@ Game.create(
 Game.create(
     name: "Tag",
     category: other,
-    description: "A game of chase wherein one player is IT and attempts to catch another player.",
-    basic_rules: "The designated tagger attempts to touch another player. By succeeding in this the other player then becomes IT and the game begins again.",
+    description: "A game of chase wherein one player is \"IT\" and attempts to catch another player.",
+    basic_rules: "The designated tagger attempts to touch another player. By succeeding in this the other player then becomes \"IT\" and the game begins again.",
     image_link: "https://upload.wikimedia.org/wikipedia/commons/4/46/Jongensspelen_10.jpg",
     min_player: 2,
     # max_player: null 
+)
+
+Game.create(
+    name: "Dungeons & Dragons",
+    category: other,
+    description: "A fantasy tabletop role-playing game (RPG)",
+    basic_rules: "One player assuming the role of \"Dungeon Master\" leads a group of players, who constitute a \"party\", through a story wherein the party's decisions and dice rolls determine the trajectory of their characters' stories and combat incidents.",
+    image_link: "https://i.pinimg.com/originals/1f/eb/29/1feb29ecb6bac1308532ed5def8f67d0.png",
+    min_player: 3
+)
+
+Game.create(
+    name: "Entwined",
+    category: video,
+    description: "A rhythm based game. The story is about two souls, a bird and a fish, that are in love but can't be together. Once united, the two souls will transform into a magnificent soaring dragon.",
+    basic_rules: "The player is tasked with guiding the two souls simultaneously, one with each analog stick, to guide them through colored rings.",
+    image_link: "https://i1.wp.com/www.geeksundergrace.com/wp-content/uploads/2014/06/entwined-screenshot-04.jpg",
+    min_player: 1,
+    max_player: 1 
+)
+
+Game.create(
+    name: "Fifa 19",
+    category: video,
+    description: "A football video game where you can play as or manage your favorite team. Special to the 2019 release: play a simulation of your favorite match up from the Women's World Cup 2019.",
+    basic_rules: "Control your favorite team in a simulated football match adhering to the rules and principles of real-life football.",
+    image_link: "https://i.pinimg.com/originals/d3/b9/1d/d3b91d768aea3665e89929fb8a596b7d.jpg",
+    min_player: 1,
+    max_player: 4
+)
+
+Game.create(
+    name: "World of Warcraft",
+    category: video,
+    description: "A fantasy based massively multiplayer online role-playing game (MMORPG).",
+    basic_rules: "Players picka  faction (Horder or Alliance), a race, and a class for their character. They then complete a series of quests, dungeons, and raids, in order to level up their character.",
+    image_link: "https://www.mobygames.com/images/shots/l/530853-world-of-warcraft-cataclysm-windows-screenshot-creating-a.jpg",
+    min_player: 1,
 )
